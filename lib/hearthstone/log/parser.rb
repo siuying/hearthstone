afa19341fc48731d4b9e486878c4f08e75a2e53e
Francis Chong <francis@ignition.hk>
2014-12-13 10:53:34 +0800

    add more event parsing

--- lib/hearthstone/log/parser.rb
+++ lib/hearthstone/log/parser.rb
@@ -1,15 +1,6 @@
 module Hearthstone
   module Log
     class Parser
-      STARTUP_REGEX     = /^Initialize engine version/
-      GAME_MODE_REGEXP  = /\[Bob\] ---(\w+)---/
-      BEGIN_SPECTATOR_REGEX = /\[Power\] .* Begin Spectating/
-      END_SPECTATOR_REGEX = /\[Power\] .* End Spectator Mode/
-      ARENA_MODE_REGEX = /\[LoadingScreen\] LoadingScreen.OnSceneLoaded\(\) - prevMode=.* currMode=DRAFT/
-      RANKED_MODE_REGEX = /.*name=rank_window.*/
-      LEGEND_RANK_REGEX = /\[Bob\] legend rank (\d*)/
-      GAME_MODE_REGEX = /\[Bob\] ---(\w+)---/
-      POWER_TAG_CHANGE_REGEX = /\[Power\] GameState.DebugPrintPower\(\) -\s*TAG_CHANGE Entity=(.*) tag=(.*) value=(.*)/
       GAME_MODE_MAPPINGS = {
         "RegisterScreenPractice" => :practice, 
         "RegisterScreenTourneys" => :casual,
@@ -22,34 +13,126 @@ module Hearthstone
 
       def parse_line(line)
         case line
-        when STARTUP_REGEX
+        when /^Initialize engine version/
           return [:startup]
-        when GAME_MODE_REGEX
+
+        when /\[Bob\] ---(\w+)---/
           mode = GAME_MODE_MAPPINGS[$1]
           return [:mode, mode] if mode
-        when BEGIN_SPECTATOR_REGEX
+
+        when /\[Power\] .* Begin Spectating/
           return [:begin_spectator_mode]
-        when END_SPECTATOR_REGEX
+
+        when /\[Power\] .* End Spectator Mode/
           return [:end_spectator_mode]
-        when ARENA_MODE_REGEX
+
+        when /\[LoadingScreen\] LoadingScreen.OnSceneLoaded\(\) - prevMode=.* currMode=DRAFT/
           return [:mode, :arena]
-        when RANKED_MODE_REGEX
+
+        when /.*name=rank_window.*/
           return [:mode, :ranked]
-        when LEGEND_RANK_REGEX
-          return [:legend, $1]
-        when POWER_TAG_CHANGE_REGEX
+
+        when /\[Bob\] legend rank (\d*)/
+          rank = $1.to_i
+          return [:legend, rank] if rank > 0
+
+
+        when /\[Power\] GameState.DebugPrintPower\(\) -\s*FULL_ENTITY.*Creating ID=(\d*) CardID=(?!GAME)(?!HERO)(.+)/
+          id = $1.to_i
+          card = $2
+          return [:open_card, id, card]
+
+        when /\[Power\] GameState.DebugPrintPower\(\) -\s*TAG_CHANGE Entity=\[.*id=(\d*).* cardId=(.*) player=(\d)\] tag=(.*) value=(.*)/
+          id = $1.to_i
+          card_id = $2
+          player = $3.to_i
+          type = $4
+          amount = $5.to_i
+          return parse_power_tag_change_entity(type, id, card_id, player, amount)
+
+        when /\[Power\] GameState.DebugPrintPower\(\) -\s*TAG_CHANGE Entity=(.*) tag=(.*) value=(.*)/
           player = $1
+          type = $2
           state = $3
-          case $2
-          when "PLAYER_ID"
-            return [:player_id, player, state.to_i]
-          when "FIRST_PLAYER"
-            return [:first_player, player]
-          when "PLAYSTATE"
-            return [:game_over, state]
-          end
+          return parse_power_tag_change(type, player, state)
+
+        when /\[Power\] GameState.SendChoices\(\) -\s*m_chosenEntities\[0\]=\[name=(.*) id=(\d*) zone=SETASIDE.*cardId=(.*) player=(\d)\]/
+          name = $1
+          id = $2.to_i
+          card_id = $3.to_i
+          player = $4.to_i
+          return [:choose, name, card_id, id, player]
+
+        when /\[Power\] GameState.DebugPrintPower\(\).*TAG_CHANGE Entity=\[name=.* id=.* zone=PLAY zonePos=0 cardId=(.*) player=(\d)\] tag=EXHAUSTED value=1/
+          card_id = $1
+          player = $2.to_i
+          return [:hero_power, card_id, player]
+
+        when /\[Zone\] ZoneChangeList\.ProcessChanges\(\) - id=(\d*) local=(.*) \[name=(.*) id=(\d*) zone=(.*) zonePos=(\d*) cardId=(.*) player=(\d*)\] zone from (.*) -> (.*)/
+          zone_id = $1
+          local = $2
+          card = $3
+          id = $4
+          card_zone = $5
+          zone_pos = $6
+          card_id = $7
+          player = $8
+          from_zone = $9
+          to_zone = $10
+          return card_change(card_zone, from_zone, to_zone, card, card_id, player.to_i, id.to_i)
+
+        when /\[Zone\] ZoneChangeList\.ProcessChanges\(\) - id=(\d*) local=(.*) \[id=(\d*) cardId=(.*) type=(.*) zone=(.*) zonePos=(\d*) player=(\d*)\] zone from (.*) -> (.*)/
+          zone_id = $1
+          local = $2
+          id = $3
+          card_id = $4
+          card_zone = $6
+          zone_pos = $7
+          card_id = $8
+          player = $9
+          from_zone = $10
+          to_zone = $11
+          return card_change(card_zone, from_zone, to_zone, "", card_id, player.to_i, id.to_i)
+
         else
+
         end
+      end
+
+      private
+      def parse_power_tag_change(type, player, state)
+        case type
+        when "PLAYER_ID"
+          return [:player_id, player, state.to_i]
+        when "FIRST_PLAYER"
+          return [:first_player, player]
+        when "PLAYSTATE"
+          return [:game_over, player, state]
+        when "TURN_START"
+          if player == "GameEntity"
+            return [:game_start]
+          else
+            return [:turn_start, player, state.to_i]
+          end
+        when "TURN"
+          return [:turn, state.to_i]
+        end
+      end
+
+      def parse_power_tag_change_entity(type, id, card_id, player, amount)
+        case type
+        when "DAMAGE"
+          return [:damaged, id: id, card_id: card_id, player: player, amount: amount]
+        when "ATTACKING"
+          return [:attack, id: id, card_id: card_id, player: player]
+        when "DEFENDING"
+          return [:attacked, id: id, card_id: card_id, player: player]
+        when "CARD_TARGET"
+          return [:card_target, id: id, card_id: card_id, player: player, target: amount]
+        end
+      end
+
+      def parse_card_change(card_zone, from_zone, to_zone, card, card_id, player, id)
       end
     end
   end
--- spec/hearthstone/log/parser_spec.rb
+++ spec/hearthstone/log/parser_spec.rb
@@ -37,4 +37,47 @@ describe Hearthstone::Log::Parser, "#parse_line" do
     expect(result).to eq([:mode, :ranked])
   end
 
+  it "returns legend rank" do
+    result = parser.parse_line("[Bob] legend rank 10")
+    expect(result).to eq([:legend, 10])
+
+    result = parser.parse_line("[Bob] legend rank 0")
+    expect(result).to be_nil
+  end
+
+  it "returns player state change" do
+    result = parser.parse_line("[Power] GameState.DebugPrintPower() - TAG_CHANGE Entity=siuying tag=PLAYER_ID value=2")
+    expect(result).to eq([:player_id, "siuying", 2])
+
+    result = parser.parse_line("[Power] GameState.DebugPrintPower() - TAG_CHANGE Entity=begize tag=FIRST_PLAYER value=1")
+    expect(result).to eq([:first_player, "begize"])
+
+    result = parser.parse_line("[Power] GameState.DebugPrintPower() - TAG_CHANGE Entity=begize tag=PLAYSTATE value=WON")
+    expect(result).to eq([:game_over, "begize", "WON"])
+
+    result = parser.parse_line("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=siuying tag=TURN_START value=1418309639")
+    expect(result).to eq([:turn_start, "siuying", 1418309639])
+
+    result = parser.parse_line("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=GameEntity tag=TURN value=3")
+    expect(result).to eq([:turn, 3])
+
+    result = parser.parse_line("[Power] GameState.DebugPrintPower() - TAG_CHANGE Entity=GameEntity tag=TURN_START value=1418310864")
+    expect(result).to eq([:game_start])
+  end
+
+  it "returns entity state change" do
+    result = parser.parse_line("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=[name=Silver Hand Recruit id=71 zone=PLAY zonePos=1 cardId=CS2_101t player=1] tag=DAMAGE value=2
+ ")
+    expect(result).to eq([:damaged, id: 71, card_id: "CS2_101t", player: 1, amount: 2])
+
+    result = parser.parse_line("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=[name=Uther Lightbringer id=4 zone=PLAY zonePos=0 cardId=HERO_04 player=1] tag=ATTACKING value=1")
+    expect(result).to eq([:attack, id: 4, card_id: "HERO_04", player: 1])
+
+    result = parser.parse_line("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=[name=Rexxar id=36 zone=PLAY zonePos=0 cardId=HERO_05 player=2] tag=DEFENDING value=1")
+    expect(result).to eq([:attacked, id: 36, card_id: "HERO_05", player: 2])
+
+    result = parser.parse_line("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=[name=Fireblast id=37 zone=PLAY zonePos=0 cardId=CS2_034 player=2] tag=CARD_TARGET value=9")
+    expect(result).to eq([:card_target, id: 37, card_id: 'CS2_034', player: 2, target: 9])
+  end
+
 end
