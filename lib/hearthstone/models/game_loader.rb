70fd25c6c34004d6588e7f4b2c855170b8ee862e
Francis Chong <francis@ignition.hk>
2014-12-14 12:08:23 +0800

    add events

--- lib/hearthstone/models/game_loader.rb
+++ lib/hearthstone/models/game_loader.rb
@@ -5,18 +5,34 @@ require_relative './game'
 module Hearthstone
   module Models
     class GameLoader
-      def self.load_from_hash(data)
+      attr_accessor :data
+      attr_reader :game
 
-        game = Game.new
+      def initialize(data)
+        @data = data
+        @game = Game.new
+      end
 
+      def load_players
         data[:players].each do |player|
           game.add_player(id: player[:id], name: player[:name], first_player: player[:first_player], 
             hero_id: player[:hero][:id], hero_card_id: player[:hero][:card_id], 
             hero_power_id: player[:hero_power][:id], hero_power_card_id: player[:hero_power][:card_id])
         end
-
-        game
       end
+
+      def load_turn(number: 0, player: nil, events: [])
+        events.each do |event|
+        end
+      end
+
+      def load_event(name, data)
+        case name
+        when :open_card, :card_received, :card_revealed
+          game.send(name, data)
+        end          
+      end
+
     end
   end
 end
--- spec/hearthstone/models/game_loader_spec.rb
+++ spec/hearthstone/models/game_loader_spec.rb
@@ -7,21 +7,58 @@ describe Hearthstone::Models::GameLoader do
     @data = JSON.parse(open(filename).read, symbolize_names: true)
   }
 
-  context "::load_from_hash" do
-    before(:all) {
-      @game = Hearthstone::Models::GameLoader.load_from_hash(@data)
-    }
+  context "#load_players" do
+    it "should load players" do
+      loader = Hearthstone::Models::GameLoader.new(@data)
+      loader.load_players
 
-    it "load players" do
-      expect(@game.players.count).to eq(2)
+      game = loader.game
+      expect(game.players.count).to eq(2)
 
-      first = @game.players.first
+      first = game.players.first
       expect(first.name).to eq("siuying")
       expect(first.hero.name).to eq("Rexxar")
 
-      second = @game.players.last
+      second = game.players.last
       expect(second.name).to eq("화이트베리")
       expect(second.hero.name).to eq("Uther Lightbringer")
     end
   end
+
+  context "Load Games" do
+    before(:each) do
+      @loader = Hearthstone::Models::GameLoader.new(@data)
+      @loader.load_players
+      @game = @loader.game
+    end
+
+    context "#load_event" do
+      it "should load open card event" do
+        @loader.load_event(:open_card, id: 5, card_id: "CS2_101")
+
+        entity = @game.entity_with_id(5)
+        expect(entity.card.id).to eq("CS2_101")
+      end
+
+      it "should load card received event" do
+        @loader.load_event(:card_received, player_id: 2, id: 7, card_id: "CS2_101")
+
+        entity = @game.entity_with_id(7)
+        expect(entity.card.id).to eq("CS2_101")
+
+        player = @game.player_with_id(2)
+        expect(player.hand).to include(entity)
+      end
+
+      it "should load card reveal event" do
+        @loader.load_event(:open_card, id: 7, card_id: "")
+        @loader.load_event(:card_revealed, id: 7, card_id: "CS2_101")
+
+        entity = @game.entity_with_id(7)
+        expect(entity.card.id).to eq("CS2_101")
+      end
+
+    end
+  end
+
 end
