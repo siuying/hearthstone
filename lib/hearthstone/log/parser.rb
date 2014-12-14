abfd1181d54aecf9ca26a2691d302734cb4c4531
Francis Chong <francis@ignition.hk>
2014-12-14 12:13:19 +0800

    change all player parameter which should be player_id

--- lib/hearthstone/log/parser.rb
+++ lib/hearthstone/log/parser.rb
@@ -58,19 +58,19 @@ module Hearthstone
           to_id = $4.to_i
           to_card_id = $5
           to_player = $6.to_i
-          return [:action_start, subtype: 'ATTACK', :from => {id: from_id, card_id: from_card_id, player: from_player}, :to => {id: to_id, card_id: to_card_id, player: to_player}]
+          return [:action_start, subtype: 'ATTACK', :from => {id: from_id, card_id: from_card_id, player_id: from_player}, :to => {id: to_id, card_id: to_card_id, player_id: to_player}]
 
         when /\[Power\] GameState.SendChoices\(\) -\s*m_chosenEntities\[0\]=\[name=(.*) id=(\d*) zone=(SETASIDE|HAND).*cardId=(.*) player=(\d)\]/
           name = $1
           id = $2.to_i
           card_id = $4
           player = $5.to_i
-          return [:choose, id: id, card_id: card_id, player: player]
+          return [:choose, id: id, card_id: card_id, player_id: player]
 
         when /\[Power\] GameState.DebugPrintPower\(\).*TAG_CHANGE Entity=\[name=.* id=.* zone=PLAY zonePos=0 cardId=(.*) player=(\d)\] tag=EXHAUSTED value=1/
           card_id = $1
           player = $2.to_i
-          return [:hero_power, card_id: card_id, player: player]
+          return [:hero_power, card_id: card_id, player_id: player]
 
         when /\[Zone\] ZoneChangeList\.ProcessChanges\(\) - id=(\d*) local=(.*) \[name=(.*) id=(\d*) zone=(.*) zonePos=(\d*) cardId=(.*) player=(\d*)\] zone from (.*) -> (.*)/
           zone_id = $1
@@ -127,13 +127,13 @@ module Hearthstone
       def parse_power_tag_change_entity(type, id, card_id, player, amount)
         case type
         when "DAMAGE"
-          return [:damaged, id: id, card_id: card_id, player: player, amount: amount] if amount > 0
+          return [:damaged, id: id, card_id: card_id, player_id: player, amount: amount] if amount > 0
         when "ATTACKING"
-          return [:attack, id: id, card_id: card_id, player: player] if amount == 1
+          return [:attack, id: id, card_id: card_id, player_id: player] if amount == 1
         when "DEFENDING"
-          return [:attacked, id: id, card_id: card_id, player: player] if amount == 1
+          return [:attacked, id: id, card_id: card_id, player_id: player] if amount == 1
         when "CARD_TARGET"
-          return [:card_target, id: id, card_id: card_id, player: player, target: amount]
+          return [:card_target, id: id, card_id: card_id, player_id: player, target: amount]
         else
           raise "unknown entity: %s, %s, %s, %s, %s" % [type, id, card_id, player, amount] if debug
         end
@@ -169,15 +169,15 @@ module Hearthstone
 
       def parse_zone_to_hand(card_zone, from_zone, to_zone, card, card_id, player, id)
         if (from_zone == "OPPOSING DECK" && to_zone == "OPPOSING HAND") || (from_zone == "FRIENDLY DECK" && to_zone == "FRIENDLY HAND")
-          return [:card_drawn, player: player, id: id, card_id: card_id]
+          return [:card_drawn, player_id: player, id: id, card_id: card_id]
         end
 
         if (from_zone == "" && to_zone == "OPPOSING HAND") || (from_zone == "" && to_zone == "FRIENDLY HAND")
-          return [:card_received, player: player, id: id, card_id: card_id]
+          return [:card_received, player_id: player, id: id, card_id: card_id]
         end
 
         if (from_zone == "FRIENDLY PLAY" && to_zone == "FRIENDLY HAND") || (from_zone == "OPPOSING PLAY" && to_zone == "OPPOSING HAND")
-          return [:card_returned, player: player, id: id, card_id: card_id]
+          return [:card_returned, player_id: player, id: id, card_id: card_id]
         end
 
         raise "unsupported hand: %s, %s, %s, %s, %s, %s, %s" % [card_zone, from_zone, to_zone, card, card_id, player, id] if debug
@@ -185,27 +185,27 @@ module Hearthstone
 
       def parse_zone_to_play(card_zone, from_zone, to_zone, card, card_id, player, id)
         if (from_zone == "OPPOSING HAND" && to_zone == "") || (from_zone == "FRIENDLY HAND" && to_zone == "")
-          return [:card_played, player: player, id: id, card_id: card_id]
+          return [:card_played, player_id: player, id: id, card_id: card_id]
         end
 
         if (from_zone == "OPPOSING HAND" && to_zone == "OPPOSING PLAY") || (from_zone == "FRIENDLY HAND" && to_zone == "FRIENDLY PLAY")
-          return [:card_played, player: player, id: id, card_id: card_id]
+          return [:card_played, player_id: player, id: id, card_id: card_id]
         end
 
         if (from_zone == "OPPOSING DECK" && to_zone == "OPPOSING PLAY") || (from_zone == "FRIENDLY DECK" && to_zone == "FRIENDLY PLAY")
-          return [:card_played, player: player, id: id, card_id: card_id]
+          return [:card_played, player_id: player, id: id, card_id: card_id]
         end
 
         if from_zone == "" && to_zone =~ /PLAY \(Hero\)/
-          return [:set_hero, player: player, id: id, card_id: card_id]
+          return [:set_hero, player_id: player, id: id, card_id: card_id]
         end
 
         if from_zone == "" && to_zone =~ /PLAY \(Hero Power\)/
-          return [:set_hero_power, player: player, id: id, card_id: card_id]
+          return [:set_hero_power, player_id: player, id: id, card_id: card_id]
         end
 
         if from_zone == "" && to_zone =~ /PLAY/
-          return [:card_put_in_play, player: player, id: id, card_id: card_id]
+          return [:card_put_in_play, player_id: player, id: id, card_id: card_id]
         end
 
         raise "unsupported play: %s, %s, %s, %s, %s, %s, %s" % [card_zone, from_zone, to_zone, card, card_id, player, id] if debug
@@ -213,11 +213,11 @@ module Hearthstone
 
       def parse_zone_to_deck(card_zone, from_zone, to_zone, card, card_id, player, id)
         if (from_zone == "FRIENDLY HAND" && to_zone == "FRIENDLY DECK") || (from_zone == "OPPOSING HAND" && to_zone == "OPPOSING DECK")
-          return [:card_reshuffled, player: player, id: id, card_id: card_id]
+          return [:card_reshuffled, player_id: player, id: id, card_id: card_id]
         end
 
         if from_zone == "" && (to_zone =~ /DECK/)
-          return [:card_added_to_deck, player: player, id: id, card_id: card_id]
+          return [:card_added_to_deck, player_id: player, id: id, card_id: card_id]
         end
 
         raise "unsupported deck: %s, %s, %s, %s, %s, %s, %s" % [card_zone, from_zone, to_zone, card, card_id, player, id] if debug
@@ -225,19 +225,19 @@ module Hearthstone
       
       def parse_zone_to_graveyard(card_zone, from_zone, to_zone, card, card_id, player, id)
         if from_zone == "" || from_zone =~ /HAND/
-          return [:card_discarded, player: player, id: id, card_id: card_id]
+          return [:card_discarded, player_id: player, id: id, card_id: card_id]
         end
 
         if from_zone =~ /DECK/
-          return [:card_discarded_from_deck, player: player, id: id, card_id: card_id]
+          return [:card_discarded_from_deck, player_id: player, id: id, card_id: card_id]
         end
 
         if from_zone =~ /PLAY \(Hero\)/
-          return [:hero_destroyed, player: player, id: id, card_id: card_id]
+          return [:hero_destroyed, player_id: player, id: id, card_id: card_id]
         end
 
         if from_zone =~ /PLAY/ || from_zone =~ /SECRET/ 
-          return [:card_destroyed, player: player, id: id, card_id: card_id]
+          return [:card_destroyed, player_id: player, id: id, card_id: card_id]
         end
 
         raise "unsupported gy: %s, %s, %s, %s, %s, %s, %s" % [card_zone, from_zone, to_zone, card, card_id, player, id] if debug
@@ -245,15 +245,15 @@ module Hearthstone
 
       def parse_zone_to_secret(card_zone, from_zone, to_zone, card, card_id, player, id)
         if (from_zone == "OPPOSING HAND" && to_zone == "OPPOSING SECRET") || (from_zone == "FRIENDLY HAND" && to_zone == "FRIENDLY SECRET")
-          return [:card_played, player: player, id: id, card_id: card_id]
+          return [:card_played, player_id: player, id: id, card_id: card_id]
         end
 
         if (from_zone == "OPPOSING DECK" && to_zone == "OPPOSING SECRET") || (from_zone == "FRIENDLY DECK" && to_zone == "FRIENDLY SECRET")
-          return [:card_put_in_play, player: player, id: id, card_id: card_id]
+          return [:card_put_in_play, player_id: player, id: id, card_id: card_id]
         end
 
         if from_zone =~ /SECRET/ && to_zone == ""
-          return [:card_revealed, player: player, id: id, card_id: card_id]
+          return [:card_revealed, player_id: player, id: id, card_id: card_id]
         end
 
         raise "unsupported secret: %s, %s, %s, %s, %s, %s, %s" % [card_zone, from_zone, to_zone, card, card_id, player, id] if debug
@@ -261,11 +261,11 @@ module Hearthstone
 
       def parse_zone_to_setaside(card_zone, from_zone, to_zone, card, card_id, player, id)
         if from_zone =~ /PLAY/
-          return [:card_setaside, player: player, id: id, card_id: card_id]
+          return [:card_setaside, player_id: player, id: id, card_id: card_id]
         end
 
         if from_zone =~ /DECK/
-          return [:card_setaside, player: player, id: id, card_id: card_id]
+          return [:card_setaside, player_id: player, id: id, card_id: card_id]
         end
 
         raise "unsupported setaside: %s, %s, %s, %s, %s, %s, %s" % [card_zone, from_zone, to_zone, card, card_id, player, id] if debug
--- spec/hearthstone/log/parser_spec.rb
+++ spec/hearthstone/log/parser_spec.rb
@@ -12,10 +12,10 @@ describe Hearthstone::Log::Parser, "#parse_line" do
     expect(result).to eq([:choice_type, "GENERAL"])
 
     result = parser.parse_line("[Power] GameState.SendChoices() -   m_chosenEntities[0]=[name=Feign Death id=38 zone=HAND zonePos=3 cardId=GVG_026 player=2]")
-    expect(result).to eq([:choose, id: 38, card_id: 'GVG_026', player: 2])
+    expect(result).to eq([:choose, id: 38, card_id: 'GVG_026', player_id: 2])
 
     result = parser.parse_line("[Power] GameState.SendChoices() -   m_chosenEntities[0]=[name=Undertaker id=8 zone=SETASIDE zonePos=0 cardId=FP1_028 player=1]")
-    expect(result).to eq([:choose, id: 8, card_id: 'FP1_028', player: 1])
+    expect(result).to eq([:choose, id: 8, card_id: 'FP1_028', player_id: 1])
   end
 
   it "returns spectator mode" do
@@ -39,7 +39,7 @@ describe Hearthstone::Log::Parser, "#parse_line" do
 
   it "returns action start" do
     result = parser.parse_line("[Power] GameState.DebugPrintPower() - ACTION_START Entity=[name=Undertaker id=59 zone=PLAY zonePos=1 cardId=FP1_028 player=2] SubType=ATTACK Index=-1 Target=[name=Rexxar id=4 zone=PLAY zonePos=0 cardId=HERO_05 player=1]")
-    expect(result).to eq([:action_start,  {:subtype=>"ATTACK", :from=>{:id=>59, :card_id=>"FP1_028", :player=>2}, :to=>{:id=>4, :card_id=>"HERO_05", :player=>1}}])
+    expect(result).to eq([:action_start,  {:subtype=>"ATTACK", :from=>{:id=>59, :card_id=>"FP1_028", :player_id=>2}, :to=>{:id=>4, :card_id=>"HERO_05", :player_id=>1}}])
   end
 
   it "returns player state change" do
@@ -65,120 +65,120 @@ describe Hearthstone::Log::Parser, "#parse_line" do
   it "returns entity state change" do
     result = parser.parse_line("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=[name=Silver Hand Recruit id=71 zone=PLAY zonePos=1 cardId=CS2_101t player=1] tag=DAMAGE value=2
  ")
-    expect(result).to eq([:damaged, id: 71, card_id: "CS2_101t", player: 1, amount: 2])
+    expect(result).to eq([:damaged, id: 71, card_id: "CS2_101t", player_id: 1, amount: 2])
 
     result = parser.parse_line("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=[name=Uther Lightbringer id=4 zone=PLAY zonePos=0 cardId=HERO_04 player=1] tag=ATTACKING value=1")
-    expect(result).to eq([:attack, id: 4, card_id: "HERO_04", player: 1])
+    expect(result).to eq([:attack, id: 4, card_id: "HERO_04", player_id: 1])
 
     result = parser.parse_line("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=[name=Rexxar id=36 zone=PLAY zonePos=0 cardId=HERO_05 player=2] tag=DEFENDING value=1")
-    expect(result).to eq([:attacked, id: 36, card_id: "HERO_05", player: 2])
+    expect(result).to eq([:attacked, id: 36, card_id: "HERO_05", player_id: 2])
 
     result = parser.parse_line("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=[name=Fireblast id=37 zone=PLAY zonePos=0 cardId=CS2_034 player=2] tag=CARD_TARGET value=9")
-    expect(result).to eq([:card_target, id: 37, card_id: 'CS2_034', player: 2, target: 9])
+    expect(result).to eq([:card_target, id: 37, card_id: 'CS2_034', player_id: 2, target: 9])
   end
 
   context "returns process change" do
     it "handles hands" do
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=47 local=False [name=Reversing Switch id=72 zone=HAND zonePos=7 cardId=PART_006 player=1] zone from  -> FRIENDLY HAND"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_received, player: 1, id: 72, card_id: "PART_006"])
+      expect(result).to eq([:card_received, player_id: 1, id: 72, card_id: "PART_006"])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=1 local=False [id=22 cardId= type=INVALID zone=HAND zonePos=4 player=1] zone from  -> OPPOSING HAND"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_received, player: 1, id: 22, card_id: ""])
+      expect(result).to eq([:card_received, player_id: 1, id: 22, card_id: ""])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=16 local=False [id=57 cardId= type=INVALID zone=HAND zonePos=0 player=2] zone from OPPOSING DECK -> OPPOSING HAND"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_drawn, player: 2, id: 57, card_id: ""])
+      expect(result).to eq([:card_drawn, player_id: 2, id: 57, card_id: ""])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=27 local=False [name=Boulderfist Ogre id=10 zone=HAND zonePos=0 cardId=CS2_200 player=1] zone from FRIENDLY DECK -> FRIENDLY HAND"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_drawn, player: 1, id: 10, card_id: 'CS2_200'])
+      expect(result).to eq([:card_drawn, player_id: 1, id: 10, card_id: 'CS2_200'])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=20 local=False [name=Leper Gnome id=26 zone=HAND zonePos=2 cardId=EX1_029 player=1] zone from OPPOSING PLAY -> OPPOSING HAND"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_returned, player: 1, id: 26, card_id: 'EX1_029'])
+      expect(result).to eq([:card_returned, player_id: 1, id: 26, card_id: 'EX1_029'])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=45 local=False [name=Harvest Golem id=10 zone=HAND zonePos=1 cardId=EX1_556 player=1] zone from FRIENDLY PLAY -> FRIENDLY HAND"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_returned, player: 1, id: 10, card_id: 'EX1_556'])
+      expect(result).to eq([:card_returned, player_id: 1, id: 10, card_id: 'EX1_556'])
     end
 
     it "handles play" do
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=1 local=True [name=Loot Hoarder id=57 zone=HAND zonePos=2 cardId=EX1_096 player=2] zone from FRIENDLY HAND -> FRIENDLY PLAY"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_played, player: 2, id: 57, card_id: 'EX1_096'])
+      expect(result).to eq([:card_played, player_id: 2, id: 57, card_id: 'EX1_096'])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=8 local=False [name=Pint-Sized Summoner id=34 zone=PLAY zonePos=1 cardId=EX1_076 player=1] zone from OPPOSING HAND -> OPPOSING PLAY"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_played, player: 1, id: 34, card_id: 'EX1_076'])
+      expect(result).to eq([:card_played, player_id: 1, id: 34, card_id: 'EX1_076'])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=84 local=False [name=Ashbringer id=84 zone=PLAY zonePos=0 cardId=EX1_383t player=1] zone from  -> OPPOSING PLAY (Weapon)"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_put_in_play, player: 1, id: 84, card_id: 'EX1_383t'])
+      expect(result).to eq([:card_put_in_play, player_id: 1, id: 84, card_id: 'EX1_383t'])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=1 local=False [name=Jaina Proudmoore id=4 zone=PLAY zonePos=0 cardId=HERO_08 player=1] zone from  -> FRIENDLY PLAY (Hero)"
       result = parser.parse_line(line)
-      expect(result).to eq([:set_hero, player: 1, id: 4, card_id: 'HERO_08'])
+      expect(result).to eq([:set_hero, player_id: 1, id: 4, card_id: 'HERO_08'])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=1 local=False [name=Gul'dan id=36 zone=PLAY zonePos=0 cardId=HERO_07 player=2] zone from  -> OPPOSING PLAY (Hero)"
       result = parser.parse_line(line)
-      expect(result).to eq([:set_hero, player: 2, id: 36, card_id: 'HERO_07'])
+      expect(result).to eq([:set_hero, player_id: 2, id: 36, card_id: 'HERO_07'])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=1 local=False [name=Steady Shot id=5 zone=PLAY zonePos=0 cardId=DS1h_292 player=1] zone from  -> FRIENDLY PLAY (Hero Power)"
       result = parser.parse_line(line)
-      expect(result).to eq([:set_hero_power, player: 1, id: 5, card_id: 'DS1h_292'])
+      expect(result).to eq([:set_hero_power, player_id: 1, id: 5, card_id: 'DS1h_292'])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=1 local=False [name=Steady Shot id=37 zone=PLAY zonePos=0 cardId=DS1h_292 player=2] zone from  -> OPPOSING PLAY (Hero Power)"
       result = parser.parse_line(line)
-      expect(result).to eq([:set_hero_power, player: 2, id: 37, card_id: 'DS1h_292'])
+      expect(result).to eq([:set_hero_power, player_id: 2, id: 37, card_id: 'DS1h_292'])
     end
 
     it "handles deck" do
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=2 local=False [name=Sludge Belcher id=46 zone=DECK zonePos=3 cardId=FP1_012 player=2] zone from FRIENDLY HAND -> FRIENDLY DECK"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_reshuffled, player: 2, id: 46, card_id: "FP1_012"])
+      expect(result).to eq([:card_reshuffled, player_id: 2, id: 46, card_id: "FP1_012"])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=3 local=False [id=69 cardId= type=INVALID zone=DECK zonePos=2 player=2] zone from OPPOSING HAND -> OPPOSING DECK"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_reshuffled, player: 2, id: 69, card_id: ""])
+      expect(result).to eq([:card_reshuffled, player_id: 2, id: 69, card_id: ""])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=1 local=False [id=38 cardId= type=INVALID zone=DECK zonePos=0 player=2] zone from  -> FRIENDLY DECK"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_added_to_deck, player: 2, id: 38, card_id: ""])
+      expect(result).to eq([:card_added_to_deck, player_id: 2, id: 38, card_id: ""])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=1 local=False [id=6 cardId= type=INVALID zone=DECK zonePos=0 player=1] zone from  -> OPPOSING DECK"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_added_to_deck, player: 1, id: 6, card_id: ""])
+      expect(result).to eq([:card_added_to_deck, player_id: 1, id: 6, card_id: ""])
     end
 
     it "handles graveyard" do
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=21 local=False [name=Webspinner id=65 zone=GRAVEYARD zonePos=2 cardId=FP1_011 player=2] zone from FRIENDLY PLAY -> FRIENDLY GRAVEYARD"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_destroyed, player: 2, id: 65, card_id: "FP1_011"])
+      expect(result).to eq([:card_destroyed, player_id: 2, id: 65, card_id: "FP1_011"])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=42 local=False [name=Animal Companion id=50 zone=GRAVEYARD zonePos=0 cardId=NEW1_031 player=2] zone from  -> OPPOSING GRAVEYARD"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_discarded, player: 2, id: 50, card_id: "NEW1_031"])
+      expect(result).to eq([:card_discarded, player_id: 2, id: 50, card_id: "NEW1_031"])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=86 local=False [name=Uther Lightbringer id=4 zone=GRAVEYARD zonePos=0 cardId=HERO_04 player=1] zone from OPPOSING PLAY (Hero) -> OPPOSING GRAVEYARD"
       result = parser.parse_line(line)
-      expect(result).to eq([:hero_destroyed, player: 1, id: 4, card_id: "HERO_04"])
+      expect(result).to eq([:hero_destroyed, player_id: 1, id: 4, card_id: "HERO_04"])
 
     end
 
     it "handles secrets" do
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=80 local=False [id=30 cardId= type=INVALID zone=SECRET zonePos=0 player=1] zone from OPPOSING DECK -> OPPOSING SECRET"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_put_in_play, player: 1, id: 30, card_id: ''])
+      expect(result).to eq([:card_put_in_play, player_id: 1, id: 30, card_id: ''])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=29 local=False [name=Explosive Trap id=43 zone=SECRET zonePos=0 cardId=EX1_610 player=2] zone from FRIENDLY DECK -> FRIENDLY SECRET"
       result = parser.parse_line(line)
-      expect(result).to eq([:card_put_in_play, player: 2, id: 43, card_id: 'EX1_610'])
+      expect(result).to eq([:card_put_in_play, player_id: 2, id: 43, card_id: 'EX1_610'])
 
       line = "[Zone] ZoneChangeList.ProcessChanges() - id=45 local=False [name=Freezing Trap id=66 zone=SETASIDE zonePos=0 cardId=EX1_611 player=2] zone from FRIENDLY DECK -> "
       result = parser.parse_line(line)
-      expect(result).to eq([:card_setaside, player: 2, id: 66, card_id: 'EX1_611'])
+      expect(result).to eq([:card_setaside, player_id: 2, id: 66, card_id: 'EX1_611'])
     end
   end
 
